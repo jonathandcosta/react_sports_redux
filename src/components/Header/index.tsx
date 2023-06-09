0da1bad98fdd65c502023f67f924ee0cf385e295
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-06-08 22:00:51 -0300

    finalizando o projeto com Redux

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,31 @@
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
 import * as S from './styles'
 
-import { Produto } from '../../App'
-
-import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
+import cesta from '../../assets/cesta.png'
 
-type Props = {
-  itensNoCarrinho: Produto[]
-  favoritos: Produto[]
-}
-
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+const Header = () => {
+  const itens = useSelector((state: RootReducer) => state.carrinho.itens)
+  const itensFavoritos = useSelector(
+    (state: RootReducer) => state.favorito.itens
+  )
+  const valorTotal = itens.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
-
   return (
     <S.Header>
-      <h1>EBAC Sports</h1>
+      <h1>Sports Style</h1>
       <div>
-        <span>{favoritos.length} favoritos</span>
+        <span>{itensFavoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
   )
 }
-
 export default Header
